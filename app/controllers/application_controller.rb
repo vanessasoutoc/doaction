600f04f6ad044e24422ae75d447e1b230d1446cc
vanessasoutoc <vanessasoutoc@gmail.com>
2018-02-26 23:37:45 -0300

    but -mensagem deve ser enviada de usuario p usuario, id enviado era da ong

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,11 @@
 class ApplicationController < ActionController::Base
     protect_from_forgery with: :exception
 
-    before_action :set_message
+    before_action :set_messages
 
     private 
 
-    def set_message
+    def set_messages
         # @countMessageNotRead = Message.where('receiver_id = ? and read = ?', current_user.id, false).count
         # puts @countMessageNotRead
         if(current_user.present?)
